c3ffb28f71c233760cf6ea8b500416708cee0be0
nishimototo <dkspm2436@gmail.com>
2021-07-25 05:51:55 +0000

    モデル修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,4 +16,8 @@ class User < ApplicationRecord
   has_many :followings, through: :active_relationships, source: :follower
   has_many :passive_relationships, class_name: "Relationship", foreign_key: :follower_id
   has_many :followers, through: :passive_relationships, source: :following
+
+  def followed_by?(user)
+    passive_relationships.where(following_id: user.id).exists?
+  end
 end
