75c188c7272e9f58ad2f271b4e4579c038128968
nishimototo <dkspm2436@gmail.com>
2021-07-25 05:57:46 +0000

    ルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -4,10 +4,15 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "homes#top"
   get "home/about" => "homes#about"
-  resources :users
-
+  resources :users do
+    resource :relationships, only: [:create, :destroy]
+    get :follows, on: :member
+    get :followers, on: :member
+  end
   resources :books do
     resource :favorites, only: [:create, :destroy]
     resources :book_comments, only: [:create, :destroy]
   end
+
+
 end
