f7dfdc11dd55a42a9bf750e70b35a0cf061d3a1a
nishimototo <dkspm2436@gmail.com>
2021-07-28 13:38:06 +0000

    7d終了

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_27_072437) do
+ActiveRecord::Schema.define(version: 2021_07_28_132245) do
 
   create_table "book_comments", force: :cascade do |t|
     t.integer "user_id"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 2021_07_27_072437) do
     t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.float "rate", default: 0.0, null: false
   end
 
   create_table "chats", force: :cascade do |t|
