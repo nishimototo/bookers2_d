fb529aa5934c12e1b164e8acc2127656eaac679c
nishimototo <dkspm2436@gmail.com>
2021-08-02 04:08:38 +0000

    バリデーション編集

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,5 +1,7 @@
 class Group < ApplicationRecord
   attachment :image
+  validates :name, presence: true
+  validates :introduction, presence: true
 
   has_many :group_users
   has_many :users, through: :group_users
